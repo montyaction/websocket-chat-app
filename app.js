170dcfe0d6b8dddf272f0eed52ad1f3d1e7b6c6f
montyaction <montyaction@hotmail.com>
2024-05-08 09:28:34 +0530

    Server side is complete

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+const PORT = process.env.PORT || 8080
+const server = app.listen(PORT, () => console.log(`🗨️  server on port http://localhost:${PORT}`))
 
+const io = require('socket.io')(server)
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+io.on('connection', (socket) => {
+    console.log(socket.id)
+})
+
+let socketConnected = new Set()
+
+io.on('connection', onConnected)
+
+function onConnected(socket) {
+    console.log(socket.id)
+    socketConnected.add(socket.id)
+
+    io.emit('clients-total', socketConnected.size)
+    
+    socket.on('disconnect', () => {
+        console.log('Socket disconnected', socket.id)
+        socketConnected.delete(socket.id)
+        io.emit('clients-total', socketConnected.size)
+    })
+};
